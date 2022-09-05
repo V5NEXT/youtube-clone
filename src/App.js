660a8bcb3670582bc961e1dea9dbd8cbc2e6d0fc
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-05 22:16:11 +0200

    Added CSS file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Box } from '@mui/system';
+import {Feed, VideoDetail, ChannelDetail, SearchFeed, Navbar} from './components'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+<BrowserRouter>
+<Box sx={{backgroundColor : '#000'}}>
+    <Navbar/>
+    <Routes>
+        <Route path='/' exact element={<Feed/>}/>
+        <Route path='/video/:id' element={<VideoDetail/>} />
+        <Route path='/channel/:id' element={<ChannelDetail/>} />
+        <Route path='/search/:searchTerm' element={<SearchFeed/>} />
+    </Routes>
+</Box>
+
+</BrowserRouter>
+
+
 }
 
 export default App
