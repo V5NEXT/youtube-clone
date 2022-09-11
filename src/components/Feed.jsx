67541defa45f7cec327ee1f71beaa177fd9ed3d6
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-11 23:22:17 +0200

    Added Typography

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,8 +5,14 @@ const Feed = () => {
   return (
     <Stack sx={{flexDirection: {sx: "colomn", md: "row"}}}>
       <Box sx={{height : {sx: 'auto', md:'92vh'}, borderRight:'1px solid #3d3d3d',
-      px : {sx:0, md:2}}}></Box>
+      px : {sx:0, md:2}}}>
+      <Typography className="copyright"
+        variant="body2" sx={{mt: 1.5, color:'#fff'}}>
+          Copyright 2022 Utube Clone
+        </Typography>
 
+      </Box>
+  
     </Stack>
   )
 }
