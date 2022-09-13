2f5594a01921b2eed80c0aceb61147dea9632717
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-14 00:06:37 +0200

    Side bar completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ const Sidebar = () => (
     <button className='category-btn'
     style={{background: category.name === selectedCategory && '#FC1503', color: 'white'}} key={category.name}>
       <span style={{color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{category.icon}</span>
-      <span>{category.name}</span>
+      <span style={{opacity: category.name === selectedCategory ? 1 : 0.8}}>{category.name}</span>
     </button>
    ))}
   </Stack>
