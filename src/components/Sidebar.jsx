0cd09edd5af7e4778578102d23af196e484f49dd
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-13 11:44:59 +0200

    Added Sidebar Css

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,11 +3,13 @@ import { Stack } from '@mui/material';
 
 import { categories } from '../utils/constants';
 
+const selectedCategory = "New"
 const Sidebar = () => (
   <Stack direction="row" sx={{overflowY : "auto", height:{sx : 'auto', md: '95%'},
                               flexDirection: {md : 'column'}}}>
    {categories.map((category)=>(
-    <button>
+    <button className='category-btn'
+    style={{background: category.name === selectedCategory && 'FC1503'}}>
       <span>{category.icon}</span>
       <span>{category.name}</span>
     </button>
