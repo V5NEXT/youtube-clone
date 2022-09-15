8628d231ff922d883e633ecc99b2ea273a09d69c
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-15 22:54:06 +0200

    Creating Video Card

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { demoThumbnailUrl, demoViralUrl,
+demoVideoTitle, demoChanneUrl, demoChannelTitle } from '../utils/constants';
+import { CheckCircle } from '@mui/icons-material';
 
-const VideoCard = () => {
+
+const VideoCard = ({video}) => {
   return (
     <div>VideoCard</div>
   )
