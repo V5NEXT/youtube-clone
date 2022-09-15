c2ead61faedd59d1be540134031835455bb4d2a7
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-15 23:07:52 +0200

    Added VideoCard title

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -14,6 +14,16 @@ const VideoCard = ({video : {id : { videoId}, snippet}}) => {
     sx={{width:358, height:180}}
      image={snippet?.thumbnails?.high?.url}/>
     </Link>
+    <CardContent sx={{backgroundColor : '#1e1e1e', height:'160px'}}>
+    <Link to={videoId? `/video/${videoId}`: demoVideoUrl}>
+        <Typography variant="subtitle1" fontWeight="bold"
+         color="#fff">
+            {snippet?.title.slice(0,60) || demoVideoTitle.slice(0,60)}
+        </Typography>
+    </Link>
+
+
+    </CardContent>
 </Card>
 
     )
