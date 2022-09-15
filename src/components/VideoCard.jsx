ab8c00aa227a718c1071d8dde11190411722e9ca
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-15 23:00:06 +0200

    Video card added to feeds

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import { Typography, Card, CardContent, CardMedia } from '@mui/material';
-import { demoThumbnailUrl, demoViralUrl,
+import { demoThumbnailUrl, demoVideoUrl,
 demoVideoTitle, demoChanneUrl, demoChannelTitle } from '../utils/constants';
 import { CheckCircle } from '@mui/icons-material';
 
 
-const VideoCard = ({video}) => {
+const VideoCard = ({video : {id : { videoId}, snippet}}) => {
   return (
-    <div>VideoCard</div>
-  )
+<Card>
+    <Link to={videoId? `/video/${videoId}`: demoVideoUrl}>
+    <CardMedia alt={snippet?.title}
+    sx={{width:358, height:180}}
+     image={snippet?.thumbnails?.high?.url}/>
+    </Link>
+</Card>
+
+    )
 }
 
 export default VideoCard
