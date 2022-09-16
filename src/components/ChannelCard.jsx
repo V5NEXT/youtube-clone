385e7ee0d8ab9608bc2a142e423eb8b9d36cf99c
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-16 22:36:19 +0200

    Channel Card Box Styles fixed

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -9,7 +9,15 @@ import { demoProfilePicture } from '../utils/constants';
 
 const ChannelCard = ({channelDetail}) =>  (
 
-  <Box sx={{boxShadow:'none', borderRadius: '20px'}}>
+  <Box sx={{
+    boxShadow:'none', borderRadius: '20px',
+    display:'flex', justifyContent:'center',
+    alignItems:'center',
+    width:{xs: '356px', md:'320px'},
+    height: '326px',
+    margin: 'auto'
+    }}>
+
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
     <CardContent sx={{display:'flex', flexDirection:
   'column', justifyContent: 'center', textAlign:'center',
@@ -23,6 +31,12 @@ const ChannelCard = ({channelDetail}) =>  (
       {channelDetail?.snippet?.title}
       <CheckCircle sx={{fontSize: 12, color:'gray', ml:'5px'}}/>
     </Typography>
+    {channelDetail?.statistics?.subscriberCount &&(
+      <Typography>
+        {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()}
+      Subscribers
+      </Typography>
+    )}
       
     </CardContent>
     </Link>
