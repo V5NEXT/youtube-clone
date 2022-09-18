7a85676638c6732af633e5289d3fd7b3574b0f65
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-19 00:25:10 +0200

    Fetching Video Api

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -7,12 +7,25 @@ import {Video} from './';
 import { fetchFromApi } from '../utils/fetchFromApi';
 
 const VideoDetail = () => {
+
+  const {id} = useParams();
+  const [videoDetail, setVideoDetail] = useState(null)
+
+
+
+useEffect(()=>{
+  fetchFromApi(`videos?part=snippet,statistics&id=${id}`)
+  .then((data)=>setVideoDetail(data.items[0]))
+}, [id]);
+
+
+
   return (
     <Box minHeight="95vh">
       <Stack direction={{xs : 'column', md:'row'}}>
         <Box flex={1}>
           <Box sx={{width: '100%', position:'sticky', top:'86px'}}>
-            <ReactPlayer/>
+            <ReactPlayer />
           </Box>
 
         </Box>
