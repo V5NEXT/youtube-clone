7cfbbc9c169ed7630276a95495a73128943f3fb1
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-19 00:13:24 +0200

    Search and Display of videos completed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,11 +8,19 @@ const SearchBar = () => {
 
 const [searchTerm, setSearchTerm] = useState('');
 
+const navigate = useNavigate();
+    const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(searchTerm){
+    navigate(`/search/${searchTerm}`);
 
+    setSearchTerm('');
+    }
+}
 
 
   return (
-    <Paper component="form" onSubmit={() => {}}
+    <Paper component="form" onSubmit={handleSubmit}
     sx={{borderRadius: 20, 
     border: '1px solid #e3e3e3',
     pl: 2,
