a91e15e89ba3d1e71fd41a42061701ccf0a4c03a
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-18 23:25:57 +0200

    Gradiant Issue fixed

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,29 +1,28 @@
 import {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
-import {Videos, ChannelCard} from './'
+import {Videos, ChannelCard} from './';
 import { fetchFromApi } from '../utils/fetchFromApi';
 
 const ChannelDetail = () => {
    const [ChannelDetail, setChannelDetail] = useState(null)
    const [videos, setVideos] = useState([])
 
-
+console.log(videos)
    const {id} = useParams();
-
    useEffect(()=>{
     fetchFromApi(`channels?part=snippet&id=${id}`)
     .then((data)=>setChannelDetail(data?.items[0]));
 
     fetchFromApi(`search?channelId=${id}&part=snippet&order=date`)
     .then((data)=>setVideos(data?.items));
-   }, [id])
+   }, [id]) 
 
   return (
 <Box minHeight="95vh">
   <Box>
     <div style={{
-      background: 'background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,9,72,1) 49%, rgba(0,212,255,1) 100%)',
+      background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,9,72,1) 49%, rgba(0,212,255,1) 100%)',
       zIndex: 10,
       height:'300px'
    }}/>
