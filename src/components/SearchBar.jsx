2afb717c01a000d4347e12418bd8a6654d2dccf6
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-18 23:51:40 +0200

    Search Bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,6 +4,13 @@ import { Paper, IconButton } from "@mui/material";
 import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
+
+
+const [searchTerm, setSearchTerm] = useState('');
+
+
+
+
   return (
     <Paper component="form" onSubmit={() => {}}
     sx={{borderRadius: 20, 
@@ -13,8 +20,8 @@ const SearchBar = () => {
     mr : { sm : 5}
 }}>
 <input className="search-bar" placeholder="Search..." 
-value=""
-onChange={() => {}}/>
+value={searchTerm}
+onChange={(e) => setSearchTerm(e.target.value)}/>
 <IconButton type="submit" sx={{p:'10px', color: 'red'}}>
 <Search/>
 </IconButton>
