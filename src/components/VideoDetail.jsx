abbde47a73ea7b7ef69d183d8eec3920249dc030
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-20 21:17:54 +0200

    Intermediate Commit

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -62,13 +62,13 @@ const VideoDetail = () => {
          
 
         </Box>
-     
+        <Box px={2} py={{md:1, xs:5}} justifyContent="center" alignItems="center">
+
+<Videos videos={relatedVideos} direction="column"/>
+</Box>
 
       </Stack>
-          <Box px={2} py={{md:1, xs:5}} justifyContent="center" alignItems="center">
-
-          <Videos videos={relatedVideos}/>
-          </Box>
+     
 
     </Box>
   )
