d2a705a918a3a2249815056b1b7f64ffffadcc0a
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-09-20 18:05:39 +0200

    Video Details Information

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -13,19 +13,41 @@ const VideoDetail = () => {
 
 
 
-useEffect(()=>{
-  fetchFromApi(`videos?part=snippet,statistics&id=${id}`)
-  .then((data)=>setVideoDetail(data.items[0]))
-}, [id]);
+  useEffect(()=>{
+    fetchFromApi(`videos?part=snippet,statistics&id=${id}`)
+    .then((data)=>setVideoDetail(data.items[0]))
+  }, [id]);
 
-
+  if(!videoDetail?.snippet) return 'Loading...'
+  const {snippet : {title, channelId, channelTitle}, statistics:{
+    viewCount, likeCount
+  }} = videoDetail;
 
   return (
     <Box minHeight="95vh">
       <Stack direction={{xs : 'column', md:'row'}}>
         <Box flex={1}>
           <Box sx={{width: '100%', position:'sticky', top:'86px'}}>
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player"/>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
+          <Typography color="#fff" variant='h5' fontWeight="bold" p={2}>
+            {title}
+          </Typography>
+          <Stack direction="row" justifyContent="space-between" sx={{
+            color:"fff"
+          }} py={1} px={2}>
+              <Link to={`/channel/${channelId}`}>
+                <Typography variant={{sm: 'subtitle1', md:'h6'}} color="#fff">
+                  {channelTitle}
+                  <CheckCircle sx={{fontSize: '12px', color:'gray',ml:'5px' }}/>
+                </Typography>
+              </Link>
+              <Stack>
+                <Typography variant="body1" sx={{opacity:0.7}}
+                 color="#fff">
+                    {parseInt(viewCount).toLocaleString()} views
+                </Typography>
+              </Stack>
+          </Stack>
           </Box>
 
         </Box>
